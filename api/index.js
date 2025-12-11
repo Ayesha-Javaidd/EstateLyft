07ea06cc9505e120dbfd6510a3d62450ff58715f
Ayesha Javaid <ayeshajavaid5005@gmail.com>
2025-12-11 08:46:32 +0500

    created user model

--- api/index.js
+++ api/index.js
@@ -1,17 +1,27 @@
 import express from "express";
-import mongoose  from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
-
+import router from "./routes/user-router";
 dotenv.config();
 
-mongoose.connect(process.env.MONGOURI).then(()=> {
-    console.log('Connected to MongoDB')
-}).catch((err) =>
-{
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 const app = express();
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello World",
+  });
+});
+
+app.use("/api/user", router);
